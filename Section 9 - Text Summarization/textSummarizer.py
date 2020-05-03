05110cffe14dd3e887313daf05e456adad924f95
Daanish Rasheed <daanishrasheed@gmail.com>
2020-05-03 16:44:01 -0400

    more things

--- Section 9 - Text Summarization/textSummarizer.py
+++ Section 9 - Text Summarization/textSummarizer.py
@@ -4,24 +4,24 @@
 # Install lxml - pip install lxml
 
 # Importing the libraries
+
 import bs4 as bs
 import urllib.request
 import re
 import nltk
-nltk.download('stopwords')
+import heapq
+# nltk.download('stopwords')
 
-# Gettings the data source
+# Getting the data
 source = urllib.request.urlopen('https://en.wikipedia.org/wiki/Global_warming').read()
 
-# Parsing the data/ creating BeautifulSoup object
 soup = bs.BeautifulSoup(source,'lxml')
 
-# Fetching the data
 text = ""
 for paragraph in soup.find_all('p'):
     text += paragraph.text
 
-# Preprocessing the data
+# Preprocessing the text
 text = re.sub(r'\[[0-9]*\]',' ',text)
 text = re.sub(r'\s+',' ',text)
 clean_text = text.lower()
@@ -29,13 +29,11 @@ clean_text = re.sub(r'\W',' ',clean_text)
 clean_text = re.sub(r'\d',' ',clean_text)
 clean_text = re.sub(r'\s+',' ',clean_text)
 
-# Tokenize sentences
+
 sentences = nltk.sent_tokenize(text)
 
-# Stopword list
 stop_words = nltk.corpus.stopwords.words('english')
 
-# Word counts 
 word2count = {}
 for word in nltk.word_tokenize(clean_text):
     if word not in stop_words:
@@ -44,12 +42,10 @@ for word in nltk.word_tokenize(clean_text):
         else:
             word2count[word] += 1
 
-# Converting counts to weights
-max_count = max(word2count.values())
 for key in word2count.keys():
-    word2count[key] = word2count[key]/max_count    
+    word2count[key] = word2count[key]/max(word2count.values())
 
-# Product sentence scores    
+
 sent2score = {}
 for sentence in sentences:
     for word in nltk.word_tokenize(sentence.lower()):
@@ -59,3 +55,10 @@ for sentence in sentences:
                     sent2score[sentence] = word2count[word]
                 else:
                     sent2score[sentence] += word2count[word]
+
+
+best_sentences = heapq.nlargest(5,sent2score,key=sent2score.get)
+print('---------------------------------------------------------')
+
+for sentence in best_sentences:
+    print(sentence)
